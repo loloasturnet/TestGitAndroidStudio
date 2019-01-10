5a19cec75e14b0d764a1228b3c86231ad5cf466d
loloasturnet <lolo@astur.net>
2019-01-10 05:44:33 +0100

    aaa

---
+++ app/src/main/java/com/example/manuel/testgitandroidstudio/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.manuel.testgitandroidstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Prueba
+        int x=0;
+    }
+}
